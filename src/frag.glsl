e7b4fbe8a358577e78ed558564019c607eb78e9e
crumblingstatue <radiantstatue@gmail.com>
2021-08-21 10:35:06 +0200

    Messing around I guess

--- src/frag.glsl
+++ src/frag.glsl
@@ -1,14 +1,10 @@
-vec4 firstColor = vec4(1.0,0.0,0.0,1.0); //red
-vec4 middleColor = vec4(0.0,1.0,0.0,1.0); // green
-vec4 endColor = vec4(0.0,0.0,1.0,1.0); // blue
-vec2 res = vec2(800., 600.);
+uniform vec2 res;
+uniform vec2 view_cen;
 
-void main()
-{
-    vec2 xy = gl_FragCoord.xy / res.xy;
-
-    float h = 0.5; // adjust position of middleColor
-    vec4 col = mix(mix(firstColor, middleColor, xy.x/h), mix(middleColor, endColor, (xy.x - h)/(1.0 - h)), step(h, xy.x));
-
-    gl_FragColor = vec4(col.x, col.y, col.z, 1);
+void main(){
+    vec2 pos = gl_FragCoord.xy / res.xy;
+    float r = abs(sin(pos.x + view_cen.x * 0.001));
+    float g = abs(sin(pos.y + view_cen.y * 0.001));
+    float b = abs(sin(1.0 - pos.x + view_cen.x * 0.001));
+    gl_FragColor = vec4(r, g, b, 1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ use bst::BinarySearchTree;
 use rand::{thread_rng, Rng};
 use sfml::{
     graphics::{
-        CircleShape, Color, Font, Rect, RectangleShape, RenderStates, RenderTarget, RenderWindow,
-        Shader, Text, Transformable, View,
+        CircleShape, Color, Font, PrimitiveType, Rect, RectangleShape, RenderStates, RenderTarget,
+        RenderWindow, Shader, Shape, Text, Transformable, Vertex, View,
     },
     system::Vector2,
-    window::{ContextSettings, Event, Key, Style},
+    window::{ContextSettings, Event, Key, Style, VideoMode},
 };
 
 pub mod bst;
@@ -19,14 +19,32 @@ struct Vis<'a, T> {
 
 struct VisNode<T> {
     value: T,
-    x_offset: i32,
-    y_offset: i32,
+    off_x: i32,
+    off_y: i32,
+    parent_off: Option<(i32, i32)>,
 }
 
-#[derive(Default)]
 struct BuildVisCtx {
     depth: i32,
     x_offset: i32,
+    parent_off: Option<(i32, i32)>,
+    dir_from_parent: Dir,
+}
+
+impl Default for BuildVisCtx {
+    fn default() -> Self {
+        Self {
+            depth: 0,
+            x_offset: 0,
+            parent_off: None,
+            dir_from_parent: Dir::Left,
+        }
+    }
+}
+
+enum Dir {
+    Left,
+    Right,
 }
 
 fn build_vis<T>(tree: &BinarySearchTree<T>) -> Vis<T> {
@@ -47,17 +65,20 @@ fn build_vis<T>(tree: &BinarySearchTree<T>) -> Vis<T> {
 fn build_vis_visit_node<'a, T>(
     node: &'a bst::Node<T>,
     vec: &mut Vec<VisNode<&'a T>>,
-    ctx: BuildVisCtx,
+    mut ctx: BuildVisCtx,
 ) {
     vec.push(VisNode {
         value: &node.key,
-        x_offset: ctx.x_offset,
-        y_offset: ctx.depth,
+        off_x: ctx.x_offset,
+        off_y: ctx.depth,
+        parent_off: ctx.parent_off,
     });
     if let Some(node) = &node.left {
         let ctx = BuildVisCtx {
             depth: ctx.depth + 1,
             x_offset: ctx.x_offset - 1,
+            parent_off: Some((ctx.x_offset, ctx.depth)),
+            dir_from_parent: Dir::Left,
         };
         build_vis_visit_node(node, vec, ctx);
     }
@@ -65,17 +86,21 @@ fn build_vis_visit_node<'a, T>(
         let ctx = BuildVisCtx {
             depth: ctx.depth + 1,
             x_offset: ctx.x_offset + 1,
+            parent_off: Some((ctx.x_offset, ctx.depth)),
+            dir_from_parent: Dir::Right,
         };
         build_vis_visit_node(node, vec, ctx);
     }
 }
 
 fn main() {
+    let mut ctx_settings = ContextSettings::default();
+    ctx_settings.set_antialiasing_level(8);
     let mut wnd = RenderWindow::new(
-        (800, 600),
+        VideoMode::desktop_mode(),
         "BST Visualization",
         Style::default(),
-        &ContextSettings::default(),
+        &ctx_settings,
     );
     wnd.set_vertical_sync_enabled(true);
     // Use whatever font you wanna use
@@ -86,12 +111,11 @@ fn main() {
         tree.insert(rng.gen_range(0..1000));
     }
     let vis = build_vis(&tree);
-    let Vector2 { x: wx, y: wy } = wnd.size();
-    let mut view = View::new((0., 0.).into(), (wx as f32, wy as f32).into());
+    let Vector2 { x: ww, y: wh } = wnd.size();
+    let mut view = View::new((0., 0.).into(), (ww as f32, wh as f32).into());
 
-    let bg_shader = Shader::from_memory(None, None, Some(include_str!("frag.glsl"))).unwrap();
-    let mut rs = RenderStates::default();
-    rs.set_shader(Some(&bg_shader));
+    let mut bg_shader = Shader::from_memory(None, None, Some(include_str!("frag.glsl"))).unwrap();
+    bg_shader.set_uniform_vec2("res", (ww as f32, wh as f32).into());
 
     while wnd.is_open() {
         while let Some(ev) = wnd.poll_event() {
@@ -114,7 +138,10 @@ fn main() {
         }
         let def_view = wnd.default_view().to_owned();
         wnd.set_view(&def_view);
-        let shape = RectangleShape::from_rect(Rect::new(0., 0., 800., 600.));
+        bg_shader.set_uniform_vec2("view_cen", view.center());
+        let mut rs = RenderStates::default();
+        let shape = RectangleShape::from_rect(Rect::new(0., 0., ww as f32, wh as f32));
+        rs.set_shader(Some(&bg_shader));
         wnd.draw_rectangle_shape(&shape, &rs);
         wnd.set_view(&view);
         draw_vis(&mut wnd, &font, &vis);
@@ -127,17 +154,30 @@ fn draw_vis<T: Display>(wnd: &mut RenderWindow, font: &Font, vis: &Vis<T>) {
     let mut text = Text::new("", font, node_radius - 8);
     let node_radius = node_radius as f32;
     let node_size = node_radius * 2.0;
-    let node_v_offset = node_size + 4.0;
+    let node_v_offset = node_size * 1.5;
     text.set_fill_color(Color::BLACK);
     let mut cshape = CircleShape::default();
     cshape.set_radius(node_radius);
     cshape.set_origin((node_radius, node_radius));
+    cshape.set_outline_color(Color::BLACK);
+    cshape.set_outline_thickness(-2.0);
     text.set_origin((16., 16.));
     for node in &vis.nodes {
-        let x = node.x_offset as f32 * node_size;
-        let y = node.y_offset as f32 * node_v_offset;
+        let x = node.off_x as f32 * node_size;
+        let y = node.off_y as f32 * node_v_offset;
         cshape.set_position((x, y));
+        cshape.set_fill_color(Color::rgb(220, 200, 70));
         wnd.draw(&cshape);
+        if let Some((x_off, y_off)) = node.parent_off {
+            let line = [
+                Vertex::with_pos_color(
+                    (x_off as f32 * node_size, y_off as f32 * node_v_offset).into(),
+                    Color::WHITE,
+                ),
+                Vertex::with_pos_color((x, y).into(), Color::BLACK),
+            ];
+            wnd.draw_primitives(&line, PrimitiveType::LINES, &RenderStates::default());
+        }
         text.set_position((x, y));
         text.set_string(&node.value.to_string());
         wnd.draw(&text);
